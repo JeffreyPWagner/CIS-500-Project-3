5e59d1433c5b1aabc4e9f249788f9bfcdaa37429
JeffreyPWagner <41526084+JeffreyPWagner@users.noreply.github.com>
2018-11-09 21:03:34 -0500

    abstract loan class complete

--- src/Loan.java
+++ src/Loan.java
@@ -1,4 +1,31 @@
 
 public abstract class Loan {
 
+	protected String name;
+	protected double interestRate;
+	protected int length;
+	protected double principle;
+	protected double monthlyPayment;
+	
+	public Loan (String name, double rate, int years, double amount) {
+		this.name = name;
+		this.interestRate = rate;
+		this.length = years;
+		this.principle = amount;
+	}
+	
+	public String process () {
+		calcMonthlyPayment();
+		return makeSummary();
+	}
+	
+	abstract public void calcMonthlyPayment();
+	
+	public String makeSummary() {
+		return "name: " + name + "\ninterest rate: " + interestRate + "\nlength: " + length + "\namount: " + principle + "\npayment: " + monthlyPayment;
+	}
+	
+	public String toString() {
+		return "Loan";
+	}
 }
