4de6135c817012211019c4fd9e6d6f887028abfa
JeffreyPWagner <41526084+JeffreyPWagner@users.noreply.github.com>
2018-11-09 21:27:10 -0500

    loan concrete classes complete

--- src/AmortizedLoan.java
+++ src/AmortizedLoan.java
@@ -1,4 +1,20 @@
 
-public class AmortizedLoan {
+public class AmortizedLoan extends Loan{
+
+	public AmortizedLoan(String name, double rate, int years, double amount) {
+		super(name, rate, years, amount);
+	}
+	
+	public void calcMonthlyPayment() {
+		double monthlyInterest = interestRate / 12;
+		int lengthMonths = length * 12;
+		double n = Math.pow(1 + monthlyInterest, lengthMonths);
+		
+		monthlyPayment = (principle * monthlyInterest * n) / (n - 1);
+	}
+	
+	public String toString() {
+		return "Full Amortized Loan";
+	}
 
 }
--- src/SimpleLoan.java
+++ src/SimpleLoan.java
@@ -1,4 +1,18 @@
 
-public class SimpleLoan {
+public class SimpleLoan extends Loan {
 
+	public SimpleLoan(String name, double rate, int years, double amount) {
+		super(name, rate, years, amount);
+	}
+
+	public void calcMonthlyPayment() {
+		double monthlyInterest = interestRate / 12;
+		int lengthMonths = length * 12;
+		
+		monthlyPayment = (principle * (monthlyInterest * lengthMonths + 1)) / lengthMonths;
+	}
+	
+	public String toString() {
+		return "Simple Interest Loan";
+	}
 }
