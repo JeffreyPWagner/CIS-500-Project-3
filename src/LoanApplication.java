dbac1f930f62a2c67f6bbbab87d62832caed7ac0
JeffreyPWagner <41526084+JeffreyPWagner@users.noreply.github.com>
2018-11-11 15:20:50 -0500

    PrintSpooler and Loan Application classes complete

--- src/LoanApplication.java
+++ src/LoanApplication.java
@@ -1,4 +1,49 @@
+import java.util.*;
+
 
 public class LoanApplication {
 
+	 Scanner scnr = new Scanner(System.in);
+	
+	 private Loan loan;
+	 private String summary;
+	 private String name;
+	 private String type;
+	 private double interestRate;
+	 private int length;
+	 private double principle;
+	 private boolean moreLoans = true;
+
+	 public void run() {
+		 
+		 while (moreLoans) {
+			 System.out.println("Enter applicant name");
+			 name = scnr.nextLine();
+			 System.out.println("Enter the interest rate");
+			 interestRate = scnr.nextDouble();
+			 System.out.println("Enter length of the loan in years");
+			 length = scnr.nextInt();
+			 System.out.println("Enter the amount of the loan");
+			 principle = scnr.nextDouble();
+			 System.out.println("Enter SI for simple interest or AL for amortized loan");
+			 type = scnr.nextLine();
+			 
+			 if (type.equalsIgnoreCase("SI")) {
+				 loan = new SimpleLoan(name, interestRate, length, principle);
+			 }
+			 
+			 else if (type.equalsIgnoreCase("AL")) {
+				 loan = new AmortizedLoan(name, interestRate, length, principle);
+			 }
+			 
+			 summary = loan.process();
+			 
+			 PrintSpooler.getSpooler().printDocument(summary);
+			 
+			 System.out.println("Process another loan? (Y/N)");
+			 if (scnr.nextLine().equalsIgnoreCase("N")) {
+				 moreLoans = false;
+			 }
+		 }
+	 }
 }
--- src/PrintSpooler.java
+++ src/PrintSpooler.java
@@ -1,4 +1,17 @@
 
 public class PrintSpooler {
+	private static PrintSpooler spooler;
+	
+	public static PrintSpooler getSpooler() {
+		if (spooler == null) {
+			spooler = new PrintSpooler();
+		}
+		return spooler;
+	}
+	
+	public void printDocument (String s) {
+		System.out.println("Loan Information:");
+		System.out.println(s);
+	}
 
 }
